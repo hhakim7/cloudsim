52cdf834fe706fa86d54bc9e0b725320de07a438
Hakim Hassani <hassanihakim@hotmail.fr>
2018-06-08 17:11:28 +0200

    Premier test fichier java

---
+++ javatest.java
@@ -0,0 +1,7 @@
+String str1 = new String("coucou"), str2 = new String("toutou");
+ 
+if (!str1.equals(str2))
+  System.out.println("Les deux chaînes sont différentes !");
+ 
+else
+  System.out.println("Les deux chaînes sont identiques !");
